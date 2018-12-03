ebd7668e1352b0243aafc0f9779bf804b0e0dfc2
nickkossolapov <nickkossolapov@gmail.com>
2018-12-03 15:07:09 +0200

    Update schedule data

--- scripts/process_schedule.py
+++ scripts/process_schedule.py
@@ -3,18 +3,24 @@ import json
 
 with open('raw_data.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
-    data = {}
+    data = {i: {} for i in range(1, 9)}
     current_start_time = ''
 
     for index, row in enumerate(reader):
-        split_row_index = index % 9
-        if split_row_index == 0:
+        load_shedding_level = index % 9
+        if load_shedding_level == 0:
             current_start_time = row[0]
-            data[current_start_time] = {}
 
         else:
-            groups = [int(val) for val in row[1:]]
-            data[current_start_time][str(split_row_index)] = groups
+            days = [int(val) for val in row[1:]]
+            for i, val in enumerate(days):
+                if not (val in data[load_shedding_level]):
+                    data[load_shedding_level][val] = {}
+                
+                if not (str(i+1) in data[load_shedding_level][val]):
+                    data[load_shedding_level][val][str(i+1)] = []
+
+                data[load_shedding_level][val][str(i+1)].append(current_start_time)
 
     with open('data.json', 'w') as outfile:
         json.dump(data, outfile)
