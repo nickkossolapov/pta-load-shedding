90d82388af7cf952c5bb450800abe4922894e8e9
nickkossolapov <nickkossolapov@gmail.com>
2018-12-10 14:23:00 +0200

    Restructure suburb data

--- scripts/process_suburbs.py
+++ scripts/process_suburbs.py
@@ -2,9 +2,13 @@ import csv
 import json
 
 with open('raw_suburbs.csv', 'r') as csvfile:
-    data = {}
-    for row in csv.reader(csvfile):
-        data[(row[0]).title()] = row[1]
+    suburb_data = []
+    for index, row in enumerate(csv.reader(csvfile)):
+        suburb = {}
+        suburb["id"] = index
+        suburb["label"] = (row[0]).title()
+        suburb["group"] = row[1]
+        suburb_data.append(suburb)
 
     with open('suburb_data.json', 'w') as outfile:
-        json.dump(data, outfile)
+        json.dump(suburb_data, outfile)
