513174029bb2760940048c0eb8df3a8a10c13621
Nick <nickkossolapov@gmail.com>
2019-02-11 16:37:44 +0200

    Updates

---
+++ precache-manifest.45ffcc58bff1c33fd95fdc31c49f146b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "256c55a3c52269d2102f",
+    "url": "/pta-load-shedding/static/js/runtime~main.256c55a3.js"
+  },
+  {
+    "revision": "04ac835bb9971ae99578",
+    "url": "/pta-load-shedding/static/js/main.04ac835b.chunk.js"
+  },
+  {
+    "revision": "90342af5f6aa7bb7ff1a",
+    "url": "/pta-load-shedding/static/js/1.90342af5.chunk.js"
+  },
+  {
+    "revision": "04ac835bb9971ae99578",
+    "url": "/pta-load-shedding/static/css/main.9bba494f.chunk.css"
+  },
+  {
+    "revision": "90342af5f6aa7bb7ff1a",
+    "url": "/pta-load-shedding/static/css/1.25ce16c6.chunk.css"
+  },
+  {
+    "revision": "1dd853253198f4ffb52218f5c5cb291c",
+    "url": "/pta-load-shedding/index.html"
+  }
+];
